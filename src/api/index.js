f1febe4843b3eec6a0a2856c628303d41f4616e8
Kayweb <kayprogrammer1@gmail.com>
2022-07-19 16:22:18 +0100

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,38 @@
+import axios from 'axios';   
+
+export const getPlacesData = async (type, sw, ne) => {
+    try {
+        const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+          params: {
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+          },
+          headers: {
+            'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+          }
+        });
+
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
+      params: {lon: lng, lat: lat},
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+        'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com'
+      }
+    });
+
+    return data;
+  }catch(error){
+    console.log(error)
+  }
+}
